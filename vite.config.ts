e88e197ccb65d1dbc816d7e9656964431e4e304f
danilosaiter <danilosaiter@hotmail.com>
2026-05-20 16:41:42 -0400

    feat: add allowedHosts configuration for development server

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   ],
   server: {
     host: '0.0.0.0',
+    allowedHosts: ['smart-schedule.niliotiii.software'],
     hmr: {
       host: 'localhost',
       port: 24678,
